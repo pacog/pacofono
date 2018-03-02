564b81d9eae53b1bbff2a8e4989f84150551d4c5
Paco Gomez Granados <pacogomezgranados@gmail.com>
2018-03-02 09:44:46 +0100

    Some editor config, add empty music mode selector component

--- src/containers/MainScreenContainer/index.tsx
+++ src/containers/MainScreenContainer/index.tsx
@@ -2,15 +2,17 @@ import * as React from "react";
 
 import AppLogo from "components/AppLogo";
 import Sidebar from "components/Sidebar";
+import MusicModeSelectorContainer from "containers/MusicModeSelectorContainer";
+
 import * as styles from "./style.css";
 
 const MainScreenContainer: React.SFC<{}> = (props: {}) => (
-  <div className={styles.background}>
-    <AppLogo />
-    <Sidebar>
-        TODO
-    </Sidebar>
-  </div>
+    <div className={styles.background}>
+        <AppLogo />
+        <Sidebar>
+            <MusicModeSelectorContainer />
+        </Sidebar>
+    </div>
 );
 
 export default MainScreenContainer;
---
+++ src/containers/MusicModeSelectorContainer/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import * as styles from "./style.css";
+
+const MusicModeSelectorContainer: React.SFC<{}> = (props: {}) => (
+    <div className={styles.container}>
+        
+    </div>
+);
+
+export default MusicModeSelectorContainer;
