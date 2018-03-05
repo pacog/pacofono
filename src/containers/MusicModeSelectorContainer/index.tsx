2b210ca121494645800ba00d3685ec252d0493ab
Paco Gomez Granados <pacogomezgranados@gmail.com>
2018-03-05 21:45:54 +0100

    Using component for song selector

--- src/containers/MusicModeSelectorContainer/index.tsx
+++ src/containers/MusicModeSelectorContainer/index.tsx
@@ -1,15 +1,18 @@
 import * as React from "react";
 import { connect, Dispatch } from "react-redux";
-
-import { SONG, SCALE } from "constants/musicModes";
-import ToggleWithIcon from "components/ToggleWithIcon";
 import FaMusic from "react-icons/fa/music";
 import MdLinearScale from "react-icons/md/linear-scale";
+
+import { SONG, SCALE } from "constants/musicModes";
+
 import { IRootState } from "store/reducers/root";
-import * as styles from "./style.css";
 import { getMusicMode } from "store/selectors/musicMode";
 import { actionCreators, IMusicModeActions } from "store/actions/musicMode";
 
+import SongSelectorContainer from "containers/SongSelectorContainer";
+import ToggleWithIcon from "components/ToggleWithIcon";
+
+import * as styles from "./style.css";
 interface IMusicModeSelector {
     currentMusicMode: string;
     changeMusicMode: (newMode: string) => any; // TODO find a better return type
@@ -25,7 +28,7 @@ const MusicModeSelector: React.SFC<IMusicModeSelector> = ({currentMusicMode, cha
             {
                 (currentMusicMode === SONG) &&
                 <div className={styles.musicModeInnerSelector}>
-                    Select song
+                    <SongSelectorContainer />
                 </div>
             }
         </div>
