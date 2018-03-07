b67bd081122427f40560581c58c85fcad3b13046
Paco Gomez Granados <pacogomezgranados@gmail.com>
2018-03-07 14:10:08 +0100

    Adding musci mode tests

--- src/__tests__/musicMode.store.spec.ts
+++ src/__tests__/musicMode.store.spec.ts
@@ -1,5 +1,7 @@
-import { CHANGE_MUSIC_MODE, actionCreators } from "../store/actions/musicMode";
-import { musicModeReducer } from "../store/reducers/musicMode";
+import { CHANGE_MUSIC_MODE, actionCreators } from "store/actions/musicMode";
+import { musicModeReducer } from "store/reducers/musicMode";
+import { rootReducer } from "store/reducers/root";
+import { getMusicMode } from "store/selectors/musicMode";
 
 describe("musicMode store", () => {
 
@@ -13,18 +15,30 @@ describe("musicMode store", () => {
         });
     });
 
-    // TODO: finish this test once relative route module resolving is done
     describe("reducer", () => {
-        it("should process changing values", () => {
-            expect(musicModeReducer({
+        it("should have an initial value", () => {
+            const state = rootReducer({}, { type: null });
+            expect(state.musicMode).toEqual({
                 mode: "song",
-            }, actionCreators.changeMusicMode("newMusicMode")))
+            });
+        });
+
+        it("should process changing values", () => {
+            const state = rootReducer({}, { type: null });
+            expect(musicModeReducer(state.musicMode, actionCreators.changeMusicMode("newMusicMode")))
                 .toEqual({
                     mode: "newMusicMode",
                 });
         });
     });
 
-    // TODO test selectors
+    describe("selectors", () => {
+        it("should be able to get musicMode", () => {
+            const state = rootReducer({}, { type: null });
+            expect(getMusicMode(state)).toBe("song");
+            const newState = rootReducer(state, actionCreators.changeMusicMode("newMusicMode2"));
+            expect(getMusicMode(newState)).toBe("newMusicMode2");
+        });
+    });
 
 });
--- src/store/reducers/root.ts
+++ src/store/reducers/root.ts
@@ -4,8 +4,8 @@ import { IMusicModeState, musicModeReducer } from "./musicMode";
 import { ISongsState, songsReducer } from "./songs";
 
 export interface IRootState {
-    readonly songs: ISongsState;
-    readonly musicMode: IMusicModeState;
+    readonly songs?: ISongsState;
+    readonly musicMode?: IMusicModeState;
 }
 
 export const rootReducer = combineReducers<IRootState>({
