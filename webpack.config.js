0b88b2f03b3e91eedef5e99f721fe3a74b24aa77
Paco Gomez Granados <pacogomezgranados@gmail.com>
2018-04-25 15:17:49 +0200

    Adding PostCSS

--- webpack.config.js
+++ webpack.config.js
@@ -58,6 +58,8 @@ module.exports = {
                     loader: "css-loader" // translates CSS into CommonJS
                 }, {
                     loader: "sass-loader" // compiles Sass to CSS
+                }, {
+                    loader: "postcss-loader"
                 }]
             },
 
