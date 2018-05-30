a3fe4fb6f53937a3d3141eecadeb498b42bf0e9c
Paco Gomez Granados <pacogomezgranados@gmail.com>
2018-05-30 10:25:02 +0200

    Improving isOpen songSelector selector test

--- src/__tests__/songSelector.store.spec.ts
+++ src/__tests__/songSelector.store.spec.ts
@@ -1,5 +1,8 @@
 // import { SET_CURRENT_SONG, actionCreators } from "store/actions/currentSong";
 import { OPEN_SONG_SELECTOR, CLOSE_SONG_SELECTOR, actionCreators } from "store/actions/songSelector";
+import { actionCreators as currentSongActionCreators } from "store/actions/currentSong";
+import { actionCreators as songsActions } from "store/actions/songs";
+
 import { songSelectorReducer } from "store/reducers/songSelector";
 import { rootReducer } from "store/reducers/root";
 import { isOpen } from "store/selectors/songSelector";
@@ -50,11 +53,20 @@ describe("songSelector store", () => {
             const state = rootReducer({}, { type: null });
             expect(isOpen(state)).toBe(true); // shown since there is no selected song
 
-            const newState = rootReducer(state, actionCreators.open());
-            expect(isOpen(newState)).toBe(true);
+            const song = {
+                id: "69",
+                name: "Medusas",
+                parts: ["partId"],
+            };
+            const newStatePrev = rootReducer(state, songsActions.addSong(song));
+            const newState = rootReducer(newStatePrev, currentSongActionCreators.setCurrentSong(song));
+            expect(isOpen(newState)).toBe(false);
 
-            const newState2 = rootReducer(newState, actionCreators.close());
-            expect(isOpen(newState2)).toBe(true); // shown since there is no selected song
+            const newState2 = rootReducer(newState, actionCreators.open());
+            expect(isOpen(newState2)).toBe(true);
+
+            const newState3 = rootReducer(newState2, actionCreators.close());
+            expect(isOpen(newState3)).toBe(false);
         });
     });
 });
