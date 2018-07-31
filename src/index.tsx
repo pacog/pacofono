5225dc2122f58315dffd585e65bcaba921cfe4d1
Paco Gomez Granados <pacogomezgranados@gmail.com>
2018-07-31 20:14:54 +0200

    Not using leapjs for now

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import * as Leap from "leapjs";
+// import * as Leap from "leapjs";
 
 import "./root-style/index.scss";
 import store from "./store";
-
-console.log(Leap);
 
 import MainScreenContainer from "containers/MainScreenContainer";
 
