5f886249804d5f549569e6644d0db5c5e2179371
Paco Gomez Granados <pacogomezgranados@gmail.com>
2018-08-16 15:27:56 +0200

    Correctly destroying throttled components

--- src/utils/throttleComponent.tsx
+++ src/utils/throttleComponent.tsx
@@ -11,7 +11,6 @@ interface IThrottledState {
     waitingForThrottle: boolean;
 }
 
-
 const throttleComponent = <P extends object, ChangeValueType>(
     Component: React.ComponentType<P>,
     handlerName: string,
@@ -59,6 +58,10 @@ const throttleComponent = <P extends object, ChangeValueType>(
             return <Component {...props } />;
         }
 
+        public componentWillUnmount() {
+            this.onChangeValueThrottled.cancel();
+        }
+
         private onValueChangeBeforeThrottle(newValue: ChangeValueType) {
             this.setState({
                 currentValue: newValue,
@@ -92,82 +95,3 @@ function shouldUseNewValueFromProps(nextProps: IThrottledProps, prevState: IThro
 
 
 export default throttleComponent;
-//
-
-//
-//
-//
-//     constructor(props: P) {
-//         super(props);
-//
-//         this.state = {
-//             waitingForThrottle: false,
-//             lastValueReceived: null,
-//             currentValue: props.value,
-//         };
-//     }
-//
-//     public render() {
-//         return (
-//             <div className="vertical-slider-wrapper">
-//                 <input
-//                     className="vertical-slider"
-//                     type="range"
-//                     min={this.props.min}
-//                     max={this.props.max}
-//                     step={this.props.step}
-//                     value={this.state.currentValue}
-//                     onChange={(e) => {
-//                         const newValue = parseFloat(e.target.value);
-//                         this.setState({
-//                             currentValue: newValue,
-//                             waitingForThrottle: true,
-//                         });
-//                         this.onChangeValueThrottled(newValue);
-//                     } }
-//                 />
-//             </div>
-//         );
-//     }
-//
-//
-//
-// }
-//
-
-
-
-
-//
-// export default VerticalSlider;
-
-// export default const throttleComponent = <T extends object>(handlerName: string, interval: number) {
-//     return (Target: React.ComponentClass): React.ComponentClass => {
-//         class ThrottleHandler extends React.Component {
-//             constructor(props: any) {
-//                 super(props);
-//
-//                 // console.log(props[handlerName]);
-//                 // const functionToThrottle: (...args: any[]) => void = props[handlerName];
-//                 // const throttled = throttle(functionToThrottle, interval);
-//                 //
-//                 // this[handlerName] = (e, ...rest) => {
-//                 //     if (e && typeof e.persist === "function") {
-//                 //         e.persist()
-//                 //     }
-//                 //     return throttled(e, ...rest)
-//                 // }
-//             }
-//
-//             public render() {
-//                 const newProps = {
-//                     ...this.props,
-//                     [handlerName]: (newValue: any) => { console.log(newValue); },
-//                 };
-//                 return React.createElement(Target, boolean);
-//             }
-//         }
-//
-//         return ThrottleHandler;
-//     };
-// }
