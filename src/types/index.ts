90209fc90ad48018b285a0bee31eabcb1cdf688b
Paco Gomez Granados <pacogomezgranados@gmail.com>
2018-10-24 11:10:03 +0200

    Creating types for synth params

--- src/types/index.ts
+++ src/types/index.ts
@@ -64,3 +64,78 @@ export interface IEnvelope {
     sustain: number;
     release: number;
 }
+
+export interface IAMSynthParams {
+    envelope: IEnvelope;
+    type: WaveTypes;
+    modulationType: WaveTypes;
+    modulationEnvelope: IEnvelope;
+    phase: number;
+    harmonicity: number;
+}
+
+export interface IFMSynthParams {
+    envelope: IEnvelope;
+    type: WaveTypes;
+    modulationType: WaveTypes;
+    modulationEnvelope: IEnvelope;
+    modulationIndex: number;
+    harmonicity: number;
+}
+
+export interface ISynthVoiceParams {
+    volume: number;
+    portamento: number;
+    type: WaveTypes;
+    filterEnvelope: IEnvelope;
+    envelope: IEnvelope;
+}
+
+export interface IDuoSynthParams {
+    vibratoAmmount: number;
+    vibratoRate: number;
+    harmonicity: number;
+    voice0: ISynthVoiceParams;
+    voice1: ISynthVoiceParams;
+}
+
+export enum FilterType {
+    Lowpass = "lowpass",
+    Highpass = "highpass",
+    Bandpass = "bandpass",
+    Lowshelf = "lowshelf",
+    Highshelf = "highshelf",
+    Notch = "notch",
+    Allpass = "allpass",
+    Peaking = "peaking",
+}
+
+export interface IFilter {
+    Q: number;
+    type: FilterType;
+    rolloff: number;
+}
+
+export interface IFilterEnvelope {
+    attack: number;
+    decay: number;
+    sustain: number;
+    release: number;
+    baseFrequency: number;
+    octaves: number;
+    exponent: number;
+}
+
+export interface IMonoSynthParams {
+    type: WaveTypes;
+    filter: IFilter;
+    filterEnvelope: IFilterEnvelope;
+    envelope: IEnvelope;
+}
+
+export interface ISimpleSynthParams {
+    type: WaveTypes;
+    envelope: IEnvelope;
+}
+
+export type SynthParams = IAMSynthParams | IFMSynthParams | IDuoSynthParams | IMonoSynthParams | ISimpleSynthParams;
