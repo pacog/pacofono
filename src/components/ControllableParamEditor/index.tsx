02cab49d5c73dfe72cc957dc6d4c8b0ff06b5f04
Paco Gomez Granados <pacogomezgranados@gmail.com>
2018-12-19 16:41:45 +0100

    Added default value to controllable param

--- src/components/ControllableParamEditor/index.tsx
+++ src/components/ControllableParamEditor/index.tsx
@@ -1,26 +1,46 @@
 import * as React from "react";
 
 import { IControllableParam } from "types";
+import getParamRestrictions from "constants/paramRestrictions";
 import "./style.scss";
 import ControllerParamSelector from "components/ControllerParamSelector";
+import SliderWithInput from "components/SliderWithInput";
 
 interface IControllableParamEditorProps {
     value: IControllableParam;
     onChange: (newParam: IControllableParam) => void;
 }
 
-const ControllableParamEditor: React.SFC<IControllableParamEditorProps> = (props: IControllableParamEditorProps) => (
-    <div className="controllable-param">
-        <ControllerParamSelector
-            value={ props.value.controllerParam }
-            onChange={ (newControllerParam) => {
-                props.onChange({
-                    ...props.value,
-                    controllerParam: newControllerParam,
-                });
-            }}
-        />
-    </div>
-);
+const ControllableParamEditor: React.SFC<IControllableParamEditorProps> = (props: IControllableParamEditorProps) => {
+    const restrictions = getParamRestrictions(props.value.name);
+    return (
+        <div className="controllable-param">
+            <div>
+                <div>Default value:</div>
+                <SliderWithInput
+                    value={ props.value.defaultValue }
+                    onChange={ (newDefaultValue) => {
+                        props.onChange({
+                            ...props.value,
+                            defaultValue: newDefaultValue,
+                        });
+                    }}
+                    min={ restrictions.min }
+                    max={ restrictions.max }
+                    step={ restrictions.step }
+                />
+            </div>
+            <ControllerParamSelector
+                value={ props.value.controllerParam }
+                onChange={ (newControllerParam) => {
+                    props.onChange({
+                        ...props.value,
+                        controllerParam: newControllerParam,
+                    });
+                }}
+            />
+        </div>
+    );
+};
 
 export default ControllableParamEditor;
